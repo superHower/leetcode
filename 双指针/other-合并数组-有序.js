68e2712c62a2b030536387e86cb2bf30fa6f66d1
yhw <yhw@icvdc.com>
2024-11-15 00:09:20 +0800

    找异位词折磨了我三小时，呜呜呜

--- 双指针/other-合并数组-有序.js
+++ 双指针/other-合并数组-有序.js
@@ -1,7 +1,15 @@
 
-// 【合并有序数组】
+/**
+ * other
+ * ----------------------------------------------------------------------------------------------------------
+ * 1. 合并数组-有序 const arr1 = [1, 3, 5], arr2 = [2, 12, 4, 6]; // [ 1, 2, 3, 4, 5, 6 ]
+ * 2. 颜色分类      const nums3 = [2,0,2,1,1,2] // 输出：[0,0,1,1,2,2] 解释：前面存0，中间存1，后面存2
+ * 
+ *----------------------------------------------------------------------------------------------------------*/
 
-const arr1 = [1, 3, 5], arr2 = [2, 12, 4, 6]; //[ 1, 2, 3, 4, 5, 6 ]
+
+// 【合并数组-有序】
+const arr1 = [1, 3, 5], arr2 = [2, 12, 4, 6]; // [ 1, 2, 3, 4, 5, 6 ]
 /**
  * while(没指到头) -> 谁小谁进res,并右移  -> 最后结束肯定有个剩下的，把他全部放进res
  */
@@ -19,3 +27,36 @@ function mergeSortedArraysUsingMergeSort(arr1, arr2) {
   return result;
 }
 
+// 【颜色分类】
+const nums3 = [2,0,2,1,1,2] // 输出：[0,0,1,1,2,2] 解释：前面存0，中间存1，后面存2
+/**
+ * 是2：让当前不是2， 交换right, 并--
+ * 是0：让0在1前面，  交换left , 并++，自己也走: 
+ * 是1：自己正常走
+ */
+console.log(sortColors(nums))
+function sortColors(nums) {
+  let left = -1          // p刚开始处于 左外
+  let right = nums.length // q刚开始处于 右外
+  let cur = 0
+  
+  while (cur < right) {
+    if (nums[cur] == 0) {
+      swap(cur, left + 1) // 保证：0在1前面
+      left++
+      cur++
+    } else if (nums[cur] == 2) {
+      swap(cur, right-1) // 保证： 当前不是2
+      right--
+    } else if (nums[cur] == 1) {
+      cur++ // 剩下的就是1, 直接正常走
+    }
+  }
+  return nums
+
+  function swap(a, b) {
+    let tmp = nums[a];
+    nums[a] = nums[b]
+    nums[b] = tmp;
+  }
+};
--- 双指针/窗口(3)-子串.js
+++ 双指针/窗口(3)-子串.js
@@ -1,59 +1,74 @@
 /**
- * 1. 无重复的最长子串
- * 2. 找字符串的 所有 字母异位词
- * 3. 最小覆盖 子串
- * --------------------------------------------------
+ * 窗口
+ * ----------------------------------------------------------------------------------------------------------
+ * 1. 无重复的最长子串             const s = "wspwwkew"   // 3 解释: 子串是 "wke"，长度为 3。注意，"pwke" 是子序列，不是子串。
+ * 2. 找字符串的 所有 字母异位词   const s1 = "cbaebabacd", p = "abc"  // [0,6]  解释: "abc" 的异位词： 0处 的"cba" |  6处 的"bac"
+ * 3. 最小覆盖 子串               const s2 = "ADOBECODEBANC", t = "ABC" // "BANC" 解释：最小覆盖子串 "BANC" 包含来自字符串 t 的 'A'、'B' 和 'C'。
+ *-----------------------------------------------------------------------------------------------------------
  *    for (let right = 0; right < s.length; right++) {
  *      while ( s[right]怎么怎么样 )) {
  *          s[left++]
  *      }
  *    }
- */
+ *----------------------------------------------------------------------------------------------------------- */
+
 
 // 【无重复的最长 子串】
 const s = "wspwwkew"   // 3 解释: 子串是 "wke"，长度为 3。注意，"pwke" 是子序列，不是子串。
 /**
- * 
- * 去重 -》 
+ * 容器：set
+ * s[right] 一旦和set重复，就让set删到不重复为止
  */
+console.log(lengthOfLongestSubstring(s))
 function lengthOfLongestSubstring(s) {
     let set = new Set(); // 去重
     let max = 0, left = 0;
 
     for (let right = 0; right < s.length; right++) {
-        while (set.has(s[right])) {// 异常：重复了 ->  最左边删除；左移
+        while (set.has(s[right])) {// 一旦和set重复，就让set删到不重复为止
             set.delete(s[left++]);
         }
-        set.add(s[right]);         // 正常情况 -> 从右边放入
+        set.add(s[right]);
 
         max = Math.max(max, right - left + 1); // 更新答案
+        console.log(set)
     }
     return max;
 }
 
 // 【找字符串的 所有 字母异位词】
 const s1 = "cbaebabacd", p = "abc"  // [0,6]  解释: "abc" 的异位词： 0处 的"cba" |  6处 的"bac"
-// const s = "abab", p = "ab"   // [0,1,2]      解释: "ab" 的异位词： 0 的"ab" | 1 的"ba" | 2 的 "ab" 
+/**
+ * 不定长 滑动窗口
+ * 0. cIndex('a') 表示 a的ASCLL值，也就是代表 a
+ * 1. 统计 p 的每种字母的出现次数
+ * 
+ */
+console.log("输出", findAnagrams(s1, p))
+
 function findAnagrams(s, p) {
-  let ans=[], left=0
-  let arr=new Array(26).fill(0)
+    const ans = [];
+    const cnt = new Array(6).fill(0); 
+    for (const c of p) cnt[ cIndex(c) ]++; // 统计 p 的每种字母的出现次数
+    
+    let left = 0;
+    for (let right = 0; right < s.length; right++) {
+        let r = cIndex(s[right])
+        cnt[r]--; // 右端点字母 进入窗口
+        while (cnt[r] < 0) { // 第二次碰见字母r 或者 r不是p中的字母
+            cnt[cIndex(s[left])]++; // 左端点字母l 离开窗口 --> 移动到右端点后面
+            left++;
+        }
+        if (right - left + 1 === p.length) { // s' 和 p 的每种字母的出现次数都相同
+            ans.push(left); // s' 左端点下标加入答案
+        }
+        console.log("输出", cnt, right, left)
+    }
+    return ans;
 
-  for(let s of p) arr[ cIndex(s) ]++ // arr是 p = 'abc'的 ASCLL个数隐射
-
-  for(let right=0; right < s.length; right++){   //  s = "cbaebabacd", p = "abc"
-      arr[ cIndex(s[right]) ]--
-      console.log(arr)
-      while(arr[ cIndex(s[right]) ] < 0) {
-        arr[ cIndex(s[left++]) ]++ 
-      }
-
-      if(right-left+1 >= p.length)  ans.push(left) // 找到符合的子串
-  }
-  return ans
-
-  function cIndex(s) {
-    return s.charCodeAt()-'a'.charCodeAt()
-  }
+    function cIndex(s) {
+      return s.charCodeAt()-'a'.charCodeAt()
+    }
 };
 
 // 【最小覆盖 子串】
@@ -97,3 +112,25 @@ function minWindow(s, t) {
   }
   return ansLeft < 0 ? "" : s.substring(ansLeft, ansRight + 1);
 };
+
+
+//
+
+/*
+c b a e b a b a c d
+#                   11000 
+L R                 10000
+L   R               00000   找到了异位词
+L     R             0000-1  遇到 非p中字母 --> L移到R后
+      R L           11100
+        #           10100
+        L R         00100 
+        L   R       0-1000  p中字母重复了 --》 L后移一位
+          L R       00100  
+          L   R     -10000  p中字母重复了 --》 L后移一位
+           L  R     00100
+           L    R   00000  找到了异位词
+           L      R 000-10 遇到 非p中字母 --> L移到R后
+                  R L
+                
+*/
